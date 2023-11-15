477f5b565eff0c683f983cf77057e3b5a87b5bfd
Vignesh100799 <baluvignesh422@gmail.com>
2023-11-16 00:17:43 +0530

    img

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
      
         <div className="navbar-brand">
-          <img width="121" height="53" src="https://www.guvi.in/blog/wp-content/uploads/2022/04/Guvi-blog-logo.png" alt="Guvi-blog-logo" />
+          <img width="121" height="53" src="https://asset.brandfetch.io/id3BHBKuok/idIoOmPqzs.png?updated=1694945463029" alt="Guvi-blog-logo" />
         </div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
